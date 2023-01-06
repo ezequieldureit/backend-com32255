63b8f4778bc2865bec488c145520a0d8a5413359
Ezequiel <ezequiel@MacBook-Pro-de-Ezequiel.local>
2023-01-05 23:56:00 -0300

    ejercicio de copas en clase

--- clase2-2.js
+++ clase2-2.js
@@ -1,14 +1,43 @@
 const persona = {
-    name: 'Eze',
-    lastname: 'Dure',
-    edad: 3,
-    country: 'Argentina'
-}
+  name: "Eze",
+  lastname: "Dure",
+  edad: 3,
+  country: "Argentina",
+};
 
-console.log(persona) // imprimer el objeto persona en si
+console.log(persona); // imprimer el objeto persona en si
 
-console.log(Object.keys(persona)) // imprime las claves del objeto
+console.log(Object.keys(persona)); // imprime las claves del objeto
 
-console.log(Object.values(persona)) // imprime los valores de los valores del objeto
+console.log(Object.values(persona)); // imprime los valores de los valores del objeto
 
-console.log(Object.entries(persona)) // imprime un nuevo array con las claves/valores por separado
+console.log(Object.entries(persona)); // imprime un nuevo array con las claves/valores por separado
+
+const campeonesDelMundo = [
+  {
+    argentina: 3,
+    brasil: 5,
+    uruguay: 2,
+  },
+  {
+    inglaterra: 2,
+    italia: 3,
+    alemania: 4,
+  },
+];
+
+
+const nombresDeEquipo = campeonesDelMundo.map((campeonesDelMundo) => Object.keys(campeonesDelMundo))
+console.log(`Nombre de equipos: ${nombresDeEquipo}`)
+
+const totalCopas = campeonesDelMundo
+.map((campeonesDelMundo) => Object.values(campeonesDelMundo))
+.flat()
+.reduce((acc, cur) => acc + cur, 0);
+console.log(`Total copas: ${totalCopas}`)
+
+
+
+
+
+
