d031ced8efa66899cf8b492ecf5e5e45bb3ecb94
Ezequiel <ezequiel@MacBook-Pro-de-Ezequiel.local>
2023-01-05 23:06:43 -0300

    object keys, values, entries

---
+++ clase2-2.js
@@ -0,0 +1,14 @@
+const persona = {
+    name: 'Eze',
+    lastname: 'Dure',
+    edad: 3,
+    country: 'Argentina'
+}
+
+console.log(persona) // imprimer el objeto persona en si
+
+console.log(Object.keys(persona)) // imprime las claves del objeto
+
+console.log(Object.values(persona)) // imprime los valores de los valores del objeto
+
+console.log(Object.entries(persona)) // imprime un nuevo array con las claves/valores por separado
